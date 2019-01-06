7e4a93b86e1e1e9af83d5ee41e7feb81c30f67f2
matsu0228 <m.hisa3153@gmail.com>
2019-01-06 14:51:04 +0900

    basic of component

--- App.js
+++ App.js
@@ -13,15 +13,26 @@ const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
     'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+    'Shake or pre:wss menu button for dev menu',
 });
 
 type Props = {};
 export default class App extends Component<Props> {
+  state = {
+message :"default message"
+  };
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
+      <HelloWorld message={this.state.message} />
+        <Text style={styles.hello}> Hellow world!!
+        </Text>
+        <Text style={styles.welcome} 
+        onPress={() => this.setState({message:'onPress'})}
+        >
+        {this.state.message}
+        
+        </Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
@@ -29,6 +40,15 @@ export default class App extends Component<Props> {
   }
 }
 
+class HelloWorld extends Component{
+  render(){
+    return (
+
+        <Text style={styles.welcome}>Hello Component: {this.props.message}
+        </Text>
+    )
+  }
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
---
+++ README.md
@@ -0,0 +1,8 @@
+## React Native Sandbox
+
+* debug
+```
+react-native run-ios 
+```
+
+* flex rayout: https://developer.mozilla.org/ja/docs/Web/CSS/align-content
